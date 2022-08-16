b82b681f213fda2a8346124fd8a2b511a1e98d2a
DANNER TAYLOR <DANNER_TN@HOTMAIL.COM>
2022-08-16 01:16:02 -0500

    attempted to create a randomizer

--- app.js
+++ app.js
@@ -30,13 +30,41 @@ let allProducts = [
     new product("wine-glass")
 ]
 
-let currentRound = 0
+let currentRound = 0;
 
 //function to return a random index inside the allProducts array
 function randomImage() {
     //make it return a number between 0-18 (18 is the index of the last item in allProducts)
-    return Math.floor(Math.random() * allProducts.length)
+    for (let i = 0; i < 3; i++) {
+        return Math.floor(Math.random() * allProducts.length);
+        let img = document.getElementById("productImage"); 
+        let img2 = document.getElementById("productImage2"); 
+        let img3 = document.getElementById("productImage3");
+        img.src = `img/${product.name}.jpg`;
+        img.alt = product.name;
+        img.title = product.name;
+        img2.src = `img/${product.name}.jpg`;
+        img2.alt = product.name;
+        img2.title = product.name;
+        img3.src = `img/${product.name}.jpg`;
+        img3.alt = product.name;
+        img3.title = product.name;
+    }
 }
+
+randomImage();
+
+// let randomized = [];
+
+// while (randomized.length < 3) {
+//     let random = randomImage();
+//     while (!randomized.includes (allProducts[random])) {
+//         randomized.push(allProducts[random]);
+//     }
+// };
+
+// randomized();
+
 
 // DOM Manipulation
 // let product = allProducts[randomImage()];
@@ -45,19 +73,35 @@ function randomImage() {
 
 
 // Event Listener Steps:
-let button = document.getElementById("productButton");
+let button = document.getElementById("productButton"); 
+let button2 = document.getElementById("productButton2"); 
+let button3 = document.getElementById("productButton3");
+
 button.addEventListener('click', showNewImage);
+button2.addEventListener('click', showNewImage);
+button3.addEventListener('click', showNewImage);
 
 // This is the event handler that gets invoked when we click the button
 function showNewImage() {
     // Get a random product
     let product = allProducts[randomImage()];
     // Select the img
-    let img = document.getElementById("productImage");
+    let img = document.getElementById("productImage"); 
+    let img2 = document.getElementById("productImage2"); 
+    let img3 = document.getElementById("productImage3");
     // Make the img the product
     img.src = `img/${product.name}.jpg`;
     img.alt = product.name;
     img.title = product.name;
+    // Create the img for image 2
+    img2.src = `img/${product.name}.jpg`;
+    img2.alt = product.name;
+    img2.title = product.name;
+    // Create the img for image 3
+    img3.src = `img/${product.name}.jpg`;
+    img3.alt = product.name;
+    img3.title = product.name;
+
     // Increments shown product's .clicked property
     product.clicked++;
     console.log(product);
